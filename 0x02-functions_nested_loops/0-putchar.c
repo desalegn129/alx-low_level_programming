f27a6eb0620fdb00e0069381e8c355f0be2b38e3
Desalegn Bushashe <debu.gaga.143@gmail.com>
2023-08-16 06:23:09 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
-int main(void)
+#include "main.h"
 /**
- * functin main prints out the result
- * retuns 0 while success
- */
+*main -print_putchar
+*return: 0
+*/
+int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
