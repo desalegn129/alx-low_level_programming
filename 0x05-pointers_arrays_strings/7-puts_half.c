2beb09b5512d1758ace994fa63912ee8c70613c3
Desalegn Bushashe <debu.gaga.143@gmail.com>
2023-08-21 16:59:40 -0700

    6-puts2.c ANS

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*puts_half - prints the half of a string,followed by a new line.
+*@str: string to be printed
+*/
+
+void puts_half(char *str)
+{
+int len = 0, index = 0, n;
+while (str[index++])
+len++;
+
+if ((len % 2) == 0)
+n = len / 2;
+
+else
+n = (len + 1) / 2;
+
+for (index = n ; index <= len - 1; index++)
+_putchar(str[index]);
+
+_putchar('\n');
+}
